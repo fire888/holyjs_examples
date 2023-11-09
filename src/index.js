0d72ad2d22f2ce9c5033609bcb87942b7000bd1f
vasilii <porokh.vs@gmail.com>
2023-11-10 00:05:57 +0300

    process

--- src/index.js
+++ src/index.js
@@ -25,11 +25,11 @@
 //import './12_00_tiles/12_tiles'
 //import './12_01_tilesConnect/12_tilesConnect'
 
-//import './14_00_wave/14_00_wave'
+import './14_00_wave/14_00_wave'
 //import './14_01_wave1floor/14_01_wave1floor'
 //import './14_05_wave2Floors/14_05_wave2floors'
 //import './14_08_waveInsert/14_08_waveInsert'
 
 //import './15_collisions/15_collisions'
-import './16_00_hard/structureBricks'
+//import './16_00_hard/structureBricks'
 
